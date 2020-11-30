680b6b709915d551f553eb4fe07e63c9be42efc3
Tomasz Sikorski <sikor666@gmail.com>
2020-11-30 16:18:50 +0100

    Switch to python3

--- autoptsclient_common.py
+++ autoptsclient_common.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """Common code for the auto PTS clients"""
 
--- ptsprojects/bluetoothservice/pbap.py
+++ ptsprojects/bluetoothservice/pbap.py
@@ -1,6 +1,5 @@
 """PBAP test cases"""
 
-from ptsprojects.testcase import TestCase
 from ptsprojects.bluetoothservice.btestcase import BTestCase
 
 
@@ -41,10 +40,10 @@ def test_cases(pts):
     """Returns a list of PBAP test cases pts -- Instance of PyPTS"""
 
     test_cases = [
-        # BTestCase("PBAP", "PBAP/PCE/PBD/BV-01-C"),
-        # BTestCase("PBAP", "PBAP/PCE/PBF/BV-02-I"),
-        # BTestCase("PBAP", "PBAP/PCE/PDF/BV-01-I"),
-        # BTestCase("PBAP", "PBAP/PCE/SSM/BV-01-C"),
+        BTestCase("PBAP", "PBAP/PCE/PBD/BV-01-C"),
+        BTestCase("PBAP", "PBAP/PCE/PBF/BV-02-I"),
+        BTestCase("PBAP", "PBAP/PCE/PDF/BV-01-I"),
+        BTestCase("PBAP", "PBAP/PCE/SSM/BV-01-C"),
         BTestCase("PBAP", "PBAP/PCE/SSM/BV-02-C"),
         BTestCase("PBAP", "PBAP/PCE/SSM/BV-06-C"),
     ]
